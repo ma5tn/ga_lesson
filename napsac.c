aa87856d921a2e4ff6359f29d7736c62e956953f
tanakamariko <gshm10218@gmail.com>
2016-03-16 15:36:28 +0900

    select change前

--- napsac.c
+++ napsac.c
@@ -4,6 +4,7 @@
 #define GENE_SIZE 20
 #define  MAX_WEIGHT 553
 #define MUTATION_RATE 0.05
+#define MAX_GENERATION 200
 
 typedef struct{
   int weight[20]; //重量
@@ -22,22 +23,22 @@ typedef struct{
 
 void initialize(Individual p[]);
 /* 全ての集団を出力する関数 */
-void print_all_gene(Individual p[]);
+void print_all_gene(Individual p[], int p_length);
 /* 全ての個体の適応度を計算する関数 */
-void calc_fitness(Individual p[]);
+void calc_fitness(Individual p[], int p_length);
 /* ルーレット選択で選択された個体のインデックスを返す関数 */
 int selection(Individual p[]);
 /* 交叉により子を生成する関数 */
 void crossover(int parent1_index, int parent2_index, Individual p[], Individual c[], int i);
 /* 突然変異をさせる関数 */
-void mutation(Individual p[]);
+void mutation(Individual p[], int p_length);
 /* fitness降順にソートする関数 */
-void sort(Individual p[]);
+void sort(Individual p[], int p_length);
 
 
 int main(void){
 
-  int i, j;
+  int i, j, k;
   Individual population[POPULATION_SIZE];
   Individual childs[POPULATION_SIZE];
   Individual pop_sort[POPULATION_SIZE * 2];
@@ -46,26 +47,45 @@ int main(void){
   
 
   initialize(population);
-  calc_fitness(population);
-  print_all_gene(population);
-  for(i = 0; i < 50 ; i+=2){
-    crossover(selection(population), selection(population), population, childs, i);
-  }
-  calc_fitness(childs);
-  print_all_gene(childs);
 
-  for(i = 0; i < POPULATION_SIZE; i++){
-    for(j = 0; j < GENE_SIZE; j++){
-      pop_sort[i].gene[j] = population[i].gene[j];
-    }
-    pop_sort[i].fitness = population[i].fitness;
+  for(k = 0; k < MAX_GENERATION; k++){
+     calc_fitness(population, sizeof(population)/sizeof(population[0]));
+//     print_all_gene(population, sizeof(population)/sizeof(population[0]));
+     for(i = 0; i < 50 ; i+=2){
+       crossover(selection(population), selection(population), population, childs, i);
+     }
+     calc_fitness(childs, sizeof(childs)/sizeof(childs[0]));
+    
+     for(i = 0; i < POPULATION_SIZE; i++){
+       for(j = 0; j < GENE_SIZE; j++){
+         pop_sort[i].gene[j] = population[i].gene[j];
+       }
+       pop_sort[i].fitness = population[i].fitness;
+     }
+     for(i = POPULATION_SIZE; i < POPULATION_SIZE * 2; i++){
+       for(j = 0; j < GENE_SIZE; j++){
+         pop_sort[i].gene[j] = childs[i].gene[j];
+       }
+       pop_sort[i].fitness = childs[i].fitness;
+     }
+    
+     mutation(pop_sort, sizeof(pop_sort)/sizeof(pop_sort[0]));
+     calc_fitness(pop_sort, sizeof(pop_sort)/sizeof(pop_sort[0]));
+//     print_all_gene(pop_sort, sizeof(pop_sort)/sizeof(pop_sort[0]));
+     sort(pop_sort, sizeof(pop_sort)/sizeof(pop_sort[0]));
+//     print_all_gene(pop_sort, sizeof(pop_sort)/sizeof(pop_sort[0]));
+    
+     for(i = 0; i < POPULATION_SIZE; i++){
+       for(j = 0; j < GENE_SIZE; j++){
+         population[i].gene[j] = pop_sort[i].gene[j];
+       }
+         population[i].fitness = pop_sort[i].fitness;
+     }
+    print_all_gene(population, sizeof(population)/sizeof(population[0]));
   }
-  for(i = POPULATION_SIZE; i < POPULATION_SIZE * 2; i++){
-    for(j = 0; j < GENE_SIZE; j++){
-      pop_sort[i].gene[j] = childs[i].gene[j];
-    }
-    pop_sort[i].fitness = childs[i].fitness;
-  }
+
+  print_all_gene(population, sizeof(population)/sizeof(population[0]));
+  return 0;
 
 }
 
@@ -82,12 +102,12 @@ void initialize(Individual p[]){
 }
 
 /* 全ての集団を出力する関数 */
-void print_all_gene(Individual p[]){
+void print_all_gene(Individual p[], int p_length){
   int i, j;
   Individual *p_;
   p_ = p;
 
-  for(i = 0; i < POPULATION_SIZE; i++){
+  for(i = 0; i < p_length; i++){
     printf("%2d:", i);
     for(j = 0; j < GENE_SIZE; j++){
       printf("%d ", p_[i].gene[j]);
@@ -97,12 +117,12 @@ void print_all_gene(Individual p[]){
 }
 
 /* 全ての個体の適応度を計算する関数  */
-void calc_fitness(Individual p[]){
+void calc_fitness(Individual p[], int p_length){
   int i, j;
   int f, total_weight;
   Individual *p_;
   p_ = p;
-  for(i = 0; i < POPULATION_SIZE; i++){
+  for(i = 0; i < p_length; i++){
     f = 0;
     total_weight = 0;
     for(j = 0; j < GENE_SIZE; j++){
@@ -131,7 +151,6 @@ int selection(Individual p[]){
   }
 
   rnd = rand() % (total_fitness + 1);
-  p_ = p;
   for(i = 0; i < POPULATION_SIZE; i++){
     tmp += p[i].fitness;
     if(rnd < tmp) break;
@@ -175,17 +194,37 @@ void crossover(int parent1_index, int parent2_index, Individual p[], Individual 
 }
 
 /* 突然変異をさせる関数 */
-void mutation(Individual p[]){
+void mutation(Individual p[], int p_length){
   int i, j, r;
   Individual *p_;
   p_ = p;
   int mutation_r = MUTATION_RATE * 100;
 
-  for(i = 0; i < POPULATION_SIZE; i++){
+  for(i = 0; i < p_length; i++){
     for(j = 0; j < GENE_SIZE; j++){
       r = rand() % 100;
       if(r < mutation_r){
         if(p[i].gene[j] == 0) p[i].gene[j] = 1; else p[i].gene[j] = 0;
+      }
+    }
+  }
+}
+
+/* fitness降順にソートする関数 */
+void sort(Individual p[], int p_length){
+  int i, j, k;
+  Individual tmp;
+  for(i = 0; i < p_length - 1; i++){
+    for(j = p_length - 1; i <  j; j--){
+      if(p[j - 1].fitness < p[j].fitness){
+        for(k = 0; k < GENE_SIZE; k++){
+          tmp.gene[k] = p[j].gene[k];
+          p[j].gene[k] = p[j - 1].gene[k];
+          p[j - 1].gene[k] = tmp.gene[k];
+        }
+          tmp.fitness = p[j].fitness;
+          p[j].fitness = p[j - 1].fitness;
+          p[j - 1].fitness = tmp.fitness;
       }
     }
   }
